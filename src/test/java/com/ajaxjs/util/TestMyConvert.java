c1b1a99f3985908d9b8af73a028e8b7ee45d2210
sp42@qq.com <sp42@qq.com>
2024-06-25 19:50:46 +0800

    ci

---
+++ src/test/java/com/ajaxjs/util/TestMyConvert.java
@@ -0,0 +1,32 @@
+package com.ajaxjs.util;
+
+import com.ajaxjs.util.convert.ConvertComplexValue;
+import lombok.Data;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class TestMyConvert {
+    @Data
+    public static class A {
+        private Integer a;
+    }
+
+    @Test
+    public void test() {
+        String json = "[{a:1, b:2}]";
+        Object o = ConvertComplexValue.getConvertValue().convert(json, List.class);
+        System.out.println(o);
+
+        json = "{a:1, b:2}";
+        Map map = (Map) ConvertComplexValue.getConvertValue().convert(json, Map.class);
+        System.out.println(map);
+
+        o = ConvertComplexValue.getConvertValue().convert(json, A.class);
+        System.out.println(o);
+
+        Object o1 = ConvertComplexValue.getConvertValue().convert(map, A.class);
+        System.out.println(o1);
+    }
+}
